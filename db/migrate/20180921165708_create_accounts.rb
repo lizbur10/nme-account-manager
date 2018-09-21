f525c1131c3605e7ccbdf01b4649c02d22840d4c
lizbur10 <liz.burton147@gmail.com>
2018-09-21 13:14:06 -0400

    generated Account resource

---
+++ db/migrate/20180921165708_create_accounts.rb
@@ -0,0 +1,19 @@
+class CreateAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :accounts do |t|
+      t.string :manager
+      t.string :company_name
+      t.string :company_address
+      t.string :company_city
+      t.string :ctct_email_list
+      t.string :contact_name
+      t.string :contact_email
+      t.string :contact_phone
+      t.string :delivery_day
+      t.string :delivery_time
+      t.text :special_instructions
+
+      t.timestamps
+    end
+  end
+end
