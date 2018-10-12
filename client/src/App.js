a279ae28a0fa80d25526230f11def4fd03bf380d
lizbur10 <liz.burton147@gmail.com>
2018-10-12 09:26:18 -0400

    got rid of layout component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
 import WorkplaceAccountListContainer from './containers/ManageWorkplaceAccounts/ListViewContainer';
 
 
@@ -8,9 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Layout>
-          <WorkplaceAccountListContainer />
-        </Layout>
+        <WorkplaceAccountListContainer />
       </div>
     );
   }
