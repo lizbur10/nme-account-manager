b8b1bf9ad6c3f76c00c545db04a907c048c185ec
lizbur10 <liz.burton147@gmail.com>
2018-10-13 15:37:52 -0400

    switched from edit button to links (temporarily) in workplace account list view

--- client/src/components/WorkplaceAccount/summaryInfo.js
+++ client/src/components/WorkplaceAccount/summaryInfo.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 class WorkplaceAccountSummary extends Component {
   render () {
     return (
       <div>
-        <p>{this.props.company_name}, {this.props.company_city}<button onClick={this.props.handleClick}>Edit </button></p>
+        {/* <p>{this.props.company_name}, {this.props.company_city}<button onClick={this.props.handleClick}>Edit </button></p> */}
+        <Link to={{
+          pathname: "/workplace_accounts"
+        }}>{this.props.company_name}, {this.props.company_city}</Link>
       </div>
     )
   }
 }
 
-export default WorkplaceAccountSummary;
+export default withRouter(WorkplaceAccountSummary);
