371c88e7284f1d114ed5547a9c58379a6219a62b
lizbur10 <liz.burton147@gmail.com>
2018-10-13 16:42:52 -0400

    moved routes from index.js to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
-import WorkplaceAccountListContainer from './containers/ManageWorkplaceAccounts/ListViewContainer';
+import WorkplaceAccountListContainer from './containers/WorkplaceAccounts/ListViewContainer';
+import WorkplaceAccount from './components/WorkplaceAccount/fullInfo';
 
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>9 Miles East Account Management</h1>
+        <Route exact path="/workplace_accounts/:id" component={WorkplaceAccount} />
+        <Route exact path="/" component={WorkplaceAccountListContainer} />
+        <Route exact path="/workplace_accounts" component={WorkplaceAccountListContainer} />
+      </div>
 
-const App = () => {
-  return (
-    <div>
-      <WorkplaceAccountListContainer />
-    </div>
-  );
+    );  
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import WorkplaceAccount from './components/WorkplaceAccount/fullInfo';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render((
     <Router>
-        <React.Fragment>
-            {/* SWITCH ROOT TO LOGIN PAGE; MOVE CURRENT App COMPONENT TO '/workplace_accounts */}
-            <Route path="/" component={App} /> 
-            <Route exact path="/workplace_accounts/:id" component={WorkplaceAccount} />
-        </React.Fragment>
+        <App />
     </Router>),
     document.getElementById('root')
 );
