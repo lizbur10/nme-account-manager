1576c32504877fcc35958ba9a1e9e45e8f20beac
lizbur10 <liz.burton147@gmail.com>
2018-10-23 12:21:46 -0400

    separated workplace account display by delivery day

--- client/src/components/WorkplaceAccount/listView.js
+++ client/src/components/WorkplaceAccount/listView.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 import WorkplaceAccountSummary from '../../components/WorkplaceAccount/summaryInfo';
 
@@ -7,25 +6,23 @@ class WorkplaceAccountList extends Component {
   render () {
     return (
       <div>
-        <h2>Workplace Accounts</h2>
-          <table>
-            <tbody>
-              <tr>
-                <th>Active</th><th>Day</th><th>Manager</th><th>Company Name</th><th>City</th>
-              </tr>
-              { this.props.workplaceAccounts.map(workplaceAccount => 
-              <WorkplaceAccountSummary 
-                key={workplaceAccount.id} 
-                id={workplaceAccount.id}
-                active={workplaceAccount.active}
-                delivery_day={workplaceAccount.delivery_day}
-                manager={workplaceAccount.manager["name"]}
-                company_name={workplaceAccount.company_name} 
-                company_city={workplaceAccount.company_city} 
-                toggleSwitch={this.props.toggleSwitch} />) }
-            </tbody>
-          </table>
-          <Link className="add-new-button" to="/workplace_accounts/new">Add New Account</Link>
+        <table>
+          <tbody>
+            <tr>
+              <th>Active</th><th>Day</th><th>Manager</th><th>Company Name</th><th>City</th>
+            </tr>
+            { this.props.workplaceAccounts.map(workplaceAccount => 
+            <WorkplaceAccountSummary 
+              key={workplaceAccount.id} 
+              id={workplaceAccount.id}
+              active={workplaceAccount.active}
+              delivery_day={workplaceAccount.delivery_day}
+              manager={workplaceAccount.manager["name"]}
+              company_name={workplaceAccount.company_name} 
+              company_city={workplaceAccount.company_city} 
+              toggleSwitch={this.props.toggleSwitch} />) }
+          </tbody>
+        </table>
       </div>
 
     )
--- client/src/containers/WorkplaceAccounts/ListViewContainer.js
+++ client/src/containers/WorkplaceAccounts/ListViewContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import WorkplaceAccountList from '../../components/WorkplaceAccount/listView';
 
 class WorkplaceAccountListContainer extends React.Component {
@@ -22,15 +24,37 @@ class WorkplaceAccountListContainer extends React.Component {
     persistUpdate = (id) => {
       let accountIndex = this.state.workplaceAccounts.findIndex(function(account) {
         return account.id === id;
-      })  
-        fetch('/workplace_accounts/' + id, { 
-          method: "PATCH",
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(this.state.workplaceAccounts[accountIndex])
-        })
+      })
+      fetch('/workplace_accounts/' + id, { 
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state.workplaceAccounts[accountIndex])
+      })
     }
+
+    separateDays = () => {
+      const daysArray = ['monday', 'tuesday', 'wednesday', 'thursday'];
+      const dayArray = [];
+      for (let i=0; i < daysArray.length; i++) {
+        dayArray[i] = this.state.workplaceAccounts.filter(function (e) {
+          return e.delivery_day.toLowerCase() === daysArray[i];
+        })
+      }
+      return dayArray;
+      // const managersArray = [...new Set(this.state.workplaceAccounts["manager"]["name"])];
+    }
+
+    returnDay = (dayArray) => {
+      if ( dayArray[0] ) {
+        console.log(dayArray[0]["delivery_day"])
+        return dayArray[0]["delivery_day"].charAt(0).toUpperCase() + dayArray[0]["delivery_day"].slice(1)
+            } else {
+        console.log('nope');
+        return ' ';
+      }
+    } 
 
     componentDidMount() {
       fetch('/workplace_accounts')
@@ -39,7 +63,20 @@ class WorkplaceAccountListContainer extends React.Component {
   }
 
     render() {
-      return <WorkplaceAccountList workplaceAccounts={this.state.workplaceAccounts} toggleSwitch={this.toggleSwitch} />
+      // const dayArray = this.separateDays();
+      // console.log(dayArray);
+      return (
+        <div>
+          { this.separateDays().map( (day, i) =>
+              <div key={i}>
+                <h2>{this.returnDay(day)}</h2>
+                <WorkplaceAccountList workplaceAccounts={day} toggleSwitch={this.toggleSwitch} />
+              </div> 
+          )}
+        <Link className="add-new-button" to="/workplace_accounts/new">Add New Account</Link>
+        
+        </div>
+      )
     }
   }
 
