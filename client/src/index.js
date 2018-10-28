1bfa343707c84a4d5ff915915e649e3d578d0861
lizbur10 <liz.burton147@gmail.com>
2018-10-28 14:40:37 -0400

    started to wire up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render((
-    <Router>
-        <App />
-    </Router>),
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>),
     document.getElementById('root')
 );
 registerServiceWorker();
