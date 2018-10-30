d0ee973d084dab1f8d7b226a0f27def24f615371
lizbur10 <liz.burton147@gmail.com>
2018-10-30 16:18:11 -0400

    got rid of separate rootReducer file - used combineReducers in index.js instead

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
-import rootReducer from './reducers/rootReducer';
+import workplaceAccountReducer from './reducers/workplaceAccountReducer';
+import managerReducer from './reducers/managerReducer';
+
+const rootReducer = combineReducers({
+    workplaceAccounts: workplaceAccountReducer,
+    managers: managerReducer
+  });
 
 const store = createStore(rootReducer);
 
